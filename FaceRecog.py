bdcf67e1b7bcf1d6be1dc9d61d8ce77c247dfc52
pakzan <pakzzzan@hotmail.com>
2018-09-17 16:41:57 +0800

    Customised for GPU

--- FaceRecog.py
+++ FaceRecog.py
@@ -7,8 +7,6 @@ import numpy as np
 import math
 import sys
 
-from imutils.video import WebcamVideoStream
-
 # This is a demo of running face recognition on live video from your webcam. It's a little more complicated than the
 # other example, but it includes some basic performance tweaks to make things run a lot faster:
 #   1. Process each video frame at 1/4 resolution (though still display it at full resolution)
@@ -37,34 +35,31 @@ def ReadWriteFace():
     global process_this_frame, face_locations, face_encodings, face_names
 
     # Grab a single frame of video
-    frame = video_capture.read()
+    ret, frame = video_capture.read()
 
     #Flip frame
     frame = cv2.flip(frame, 1)
-
-    # Resize frame of video to 1/4 size for faster face recognition processing
-    small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
-
+    
     # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
-    rgb_small_frame = small_frame[:,:,::-1]
+    rgb_frame = frame[:, :, ::-1]
     
     # Only process every other frame of video to save time
     if process_this_frame:
         # Find all the faces and face encodings in the current frame of video
-        face_locations = face_recognition.face_locations(rgb_small_frame)
-        face_encodings = face_recognition.face_encodings(
-            rgb_small_frame, face_locations)
+        face_locations = face_recognition.face_locations(rgb_frame)
+        # Using GPU
+        #face_locations = face_recognition.batch_face_locations(rgb_frame, 5)
+        #face_encodings = face_recognition.face_encodings(rgb_frame, face_locations, 5)
+        face_encodings = face_recognition.face_encodings(rgb_frame, face_locations)
 
         face_names = []
         for face_encoding in face_encodings:
-            # See if the face is a match for the known face(s)
-            matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
-
             name = "Unknown"
-            if True in matches:
-                face_distance = face_recognition.face_distance(known_face_encodings, face_encoding)
 
-                name_index = np.argmin(face_distance)
+            # See if the face is a match for the known face(s)
+            face_distances = face_recognition.face_distance(known_face_encodings, face_encoding)
+            if min(face_distances) < 0.5:
+                name_index = np.argmin(face_distances)
                 name = known_face_names[name_index]
                 
             face_names.append(name)
@@ -88,11 +83,6 @@ def ReadWriteFace():
 
     # Display the results
     for (top, right, bottom, left), name in zip(face_locations, face_names):
-        # Scale back up face locations since the frame we detected in was scaled to 1/4 size
-        top *= 4
-        right *= 4
-        bottom *= 4
-        left *= 4
 
         # Draw a box around the face
         cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
@@ -128,7 +118,7 @@ player_names = []
 process_this_frame = True
 start_game = False
 
-video_capture = WebcamVideoStream(src=0).start()
+video_capture = cv2.VideoCapture(0)
 
 # Load face encodings
 with open(os.getcwd() + '/classmate/dataset_faces.dat', 'rb') as f:
@@ -170,5 +160,5 @@ while True:
         break
 
 # Release handle to the webcam
-video_capture.stop()
+video_capture.release()
 cv2.destroyAllWindows()
